3790be840c41b3b594d4ad03c04525ac6675af00
junwei_tang <junwei_tang@outlook.com>
2017-08-07 16:14:26 +0800

    Update practice 2-3.c

--- practice 2-3.c
+++ practice 2-3.c
@@ -1,34 +1,59 @@
 /*＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
 K.R c
-版本 
 
 练习2-3 编写函数htoi(s)，把由十六进制数字组成的字符串（包含可选的前缀0x或0X）
 转换为与之等价的整型值。字符串中允许包含的数字包括：0～9、a~f以及A~F。
 
 例如：
 字符串 “abc123456789ABC”
-转换为  abc123456789ABC
+转换为  ascii 表对应的整型值
 ＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊*/
 
 #include <stdio.h>
-#define MAX 100 
+
+#define YES 1
+#define NO 0
+
+int htoi(char s[]);
+
 int main()
 {
     char c;
-    char s[MAX];
-    int i;
-    while ((c != getchar()) != EOF) {
-        if(c >= '0' && c <= '9')
-        {
-          
-        }
-        if(c >= 'a' && c <= 'f')
-        {}
-        if(c >= 'A' && c <= 'F')
-        {}  
+    char s[100];
+    int i = 0;
+    while ((c = getchar()) != EOF) {
+        s[i] = c;
+        i++;
     }
+    htoi (s);
+    
+}
+
+int htoi(char s[])
+{
+    int hexdigit, i , inhex , n;
+
     i = 0;
-    while (s[i] != '\0') {
-      putchar(s[i]);
+    if(s[i] == '\0')
+    {
+      ++i;
+      if(s[i] == 'x' || s[i] == 'X')
+        ++i;
     }
+    n = 0;
+    inhex = YES;
+    for(;inhex == YES; ++i)
+    {
+      if(s[i] >= '0' && s[i] <= '9')
+          hexdigit = s[i] - '0';
+      else if(s[i] >= 'a' && s[i] <= 'f')
+          hexdigit = s[i] - 'a' + 10;
+      else if(s[i] >= 'A' && s[i] <= 'F')
+          hexdigit = s[i] - 'A' + 10;
+      else
+          inhex = NO;
+      if(inhex == YES)
+          n = 16 * n + hexdigit;
+    }
+    return n;
 }
