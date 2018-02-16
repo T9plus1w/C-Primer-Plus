f8641d192d71064b45fc3c84199638d27a455cb3
junwei_tang <junwei_tang@outlook.com>
2018-02-16 13:41:43 +0800

    Create primer15-5.c

---
+++ primer15-5.c
@@ -0,0 +1,30 @@
+/*
+  编写一个函数，该函数将一个unsigned int中的所有位向左旋转指定数量的位。例如，rotate_1(x,4)
+将x中的所有位向左移动4个位置，而且从左端丢失的位会重新出现在右端。也就是说，把从高位移出的位放入
+低位。在程序中测试该函数。
+*/
+#include <stdio.h>
+
+int rotate_1(unsigned int number,unsigned int bit);
+
+int main(void)
+{
+
+  return 0;
+}
+
+//向左旋转指定数量的位
+int rotate_1(unsigned int number,unsigned int bit)
+{
+    unsigned int i;
+    unsigned int hign = 8 * sizeof(unsigned int);//最高位
+    for(i = 0;i < bit;i++)
+    {
+        if(number & (1 << (hign - 1)))
+            number = (number << 1) | 1;
+        else
+            number = number << 1;
+    }
+
+    return number;
+}
